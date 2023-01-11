e72c11776dfc41ff009df6dc15671ff3ea51f716
MizouziE <s.anglin@live.co.uk>
2023-01-11 17:18:04 +0100

    add funcs

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import ()
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	"github.com/mizouzie/crm/database"
+	"github.com/mizouzie/crm/lead"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("mysql", "leads.db")
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database migration complete")
+}
 
 func main() {
-
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(8000)
+	defer database.DBConn.Close()
 }
